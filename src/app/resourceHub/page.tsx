23eb3d8965371536b8f4e25be227550a8e5916e6
Karthik S Kailas <karthikskailas@gmail.com>
2025-10-19 19:41:31 +0530

    resourceHub page is created

---
+++ src/app/resourceHub/page.tsx
@@ -0,0 +1,14 @@
+import Header from "@/components/landing/Header"
+import Footer from "@/components/landing/Footer"
+
+export default async function ResourceHub() {
+	return (
+		<div className='flex min-h-dvh flex-col crumpled-paper'>
+			<Header />
+			<main className='flex-1'>
+			
+			</main>
+			<Footer />
+		</div>
+	)
+}
