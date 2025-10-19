0a9bd42235473e988053939588a302bf1a8387eb
Neeraj2303 <neerajsukumaran2303@gmail.com>
2025-10-19 23:31:49 +0530

    home page updated with 3 events

--- src/components/landing/Events.tsx
+++ src/components/landing/Events.tsx
@@ -14,15 +14,25 @@ const BoardImage = () => (
     />
 );
 
+const BulletPoint = () => (
+  <Image 
+      src="/images/bullet.png" 
+      alt="Bullet Point"
+      width={20}
+      height={20}
+      className="relative w-5 h-5 md:top-3 md:left-3"
+  />
+)
+
 interface UpcomingEvent {
     id: number;
     name: string;
     image: string;
 }
 
-export default function Events({ initialUpcomingEvent }: { initialUpcomingEvent: UpcomingEvent | null }) {
-  const upcomingEvent = initialUpcomingEvent;
-  const loading = !initialUpcomingEvent;
+export default function Events({ initialUpcomingEvents }: { initialUpcomingEvents: UpcomingEvent[] | null }) {
+  const upcomingEvents = initialUpcomingEvents;
+  const loading = !initialUpcomingEvents;
 
 
   return (
@@ -43,20 +53,26 @@ export default function Events({ initialUpcomingEvent }: { initialUpcomingEvent:
             viewport={{ once: true }}
             transition={{ duration: 0.6, delay: 0.2 }}
           >
-            <div className="absolute -rotate-[5deg] top-[-120] w-[1000] overflow-hidden md:w-[550] whitespace-nowrap md:overflow-visible md:-rotate-[5deg] md:top-[-220] md:left-10 text-center">
+            <div className="absolute -rotate-[5deg] top-[-120px] w-[1000px] overflow-hidden md:w-[550px] whitespace-nowrap md:overflow-visible md:-rotate-[5deg] md:top-[-220px] md:left-10 text-center">
               <div className="bg-black text-background font-black text-[25px] px-8 py-4 md:text-4xl md:px-8 md:py-4">
                 UPCOMING EVENTS
               </div>
             </div>
             {loading ? (
-              <div className="space-y-4 w-full max-w-sm mx-auto md:mx-0">
+              <div className="space-y-4 w-full max-w-sm mx-auto md:mx-0 mt-8 md:mt-0">
                 <Skeleton className="h-6 w-full" />
                 <Skeleton className="h-6 w-5/6" />
                 <Skeleton className="h-6 w-3/4" />
               </div>
-            ) : upcomingEvent ? (
-              <div className="relative font-['Comic_Neue'] text-2xl shadow-[0_5px_10px_rgba(0,0,0,0.3)] text-foreground/80 leading-relaxed space-y-2 bg-[#FFF1D6] w-[900] -rotate-[5deg] top-[-40] md:top-[-140] md:left-[12] md:w-[552] rounded-[5]">
-                  <p className="md:pl-[50] text-bold">{upcomingEvent.name}</p>
+            ) : upcomingEvents && upcomingEvents.length > 0 ? (
+              <div className="space-y-4 mt-8 md:mt-0">
+              {upcomingEvents.slice(0, 3).map((event) => (
+                <div key={event.id} className="relative font-['Comic_Neue'] text-2xl shadow-[0_5px_10px_rgba(0,0,0,0.3)] text-foreground/80 leading-relaxed space-y-2 bg-[#FFF1D6] w-[900px] -rotate-[5deg] top-[-40px] md:top-[-140px] md:left-[12px] md:w-[552px] md:h-[50px] md:pt-[5px] rounded-[5px]">
+                <BulletPoint />
+                <p className="translate-y-[-14px] md:translate-y-[-26px] md:pl-[50] text-bold">{event.name}</p>
+              </div>
+            ))}
+            <p className="relative sr-only sm:not-sr-only font-['Rock_Salt'] -rotate-[5deg] translate-x-[30%] translate-y-[-100px]">More events are on the way...</p>
               </div>
             ) : (
               <div className="font-['Comic_Neue'] text-2xl text-foreground/80 leading-relaxed space-y-2">
