087e88a40310e094743e5b762c6dda2f705a0dc1
Neeraj2303 <neerajsukumaran2303@gmail.com>
2025-10-21 20:00:19 +0530

    added a meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,13 +18,14 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="" />
         <link href="https://fonts.googleapis.com/css2?family=Kumbh+Sans:wght@400;700;900&family=Special+Elite&family=Rock+Salt&display=swap" rel="stylesheet" />
         <link href="https://fonts.cdnfonts.com/css/nimbus-sans-l" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Comic+Neue:ital,wght@0,300;0,400;0,700;1,300;1,400;1,700&family=Fascinate&family=Pacifico&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"></link>
       </head>
-      <body className="font-body antialiased crumpled-paper bg-background text-foreground min-h-dvh">
+      <body className="font-body antialiased crumpled-paper bg-background text-foreground min-h-dvh overflow-x-hidden">
         {children}
         <Toaster />
       </body>
