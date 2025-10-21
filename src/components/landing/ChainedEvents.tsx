288ef5cd71b11dba322f234e71cb426aa34acf9d
SREEHARI <sreehari14shr@gmail.com>
2025-10-21 14:55:24 +0000

    marue should be such that the chain touches it

--- src/components/landing/ChainedEvents.tsx
+++ src/components/landing/ChainedEvents.tsx
@@ -24,9 +24,9 @@ const eventItems = [
 
 export default function ChainedEvents() {
   return (
-    <section className="relative w-full pt-24 md:pt-32 pb-32 md:pb-48 -mt-1 overflow-hidden">
+    <section className="relative w-full pb-32 md:pb-48 -mt-1 overflow-hidden">
       <div className="container relative">
-        <div className="flex flex-col lg:flex-row justify-center items-start gap-16 lg:gap-24 relative">
+        <div className="flex flex-col lg:flex-row justify-center items-start gap-16 lg:gap-24 relative pt-24 md:pt-32">
           
           {/* Main content block - moved to the left */}
           <motion.div
